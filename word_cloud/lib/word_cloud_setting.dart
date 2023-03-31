05dbeb2d056f2b86b6d286b43966809be920f9ec
Jaehyun Jeong <j2hyeon02@kaist.ac.kr>
2023-03-31 03:11:23 +0000

    issue fized in package

--- word_cloud/lib/word_cloud_setting.dart
+++ word_cloud/lib/word_cloud_setting.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:word_cloud/word_cloud_shape.dart';
 
--- word_cloud/lib/word_cloud_tap_view.dart
+++ word_cloud/lib/word_cloud_tap_view.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:word_cloud/word_cloud_data.dart';
 import 'package:word_cloud/word_cloud_setting.dart';
